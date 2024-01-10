4d5ed38b9608040bcccd7344b826fd574d09e78c
Sergey <79226106542@ya.ru>
2024-01-10 09:12:11 +0500

    add scripts

---
+++ scripts/tests/test1.py
@@ -0,0 +1,13 @@
+import pickle
+import pandas as pd
+
+df = pd.read_csv("/home/stage-srv/Titanic/datasets/stage4/test.csv")
+X = df.iloc[:,[1,2,3]]
+y = df.iloc[:,0]
+
+def test1():
+    with open("/home/stage-srv/Titanic/models/model.pkl", "rb") as fd:
+        clf = pickle.load(fd)
+    score = clf.score(X, y)
+    assert score > 0.5
+
